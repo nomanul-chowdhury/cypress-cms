eb9ed5c935685adb8933a61765a94de71599d8be
nomanul-chowdhury <noman_03@hotmail.com>
2023-08-13 10:10:22 +1000

    Update on step def

--- cypress/integration/common/findNSW.js
+++ cypress/integration/common/findNSW.js
@@ -4,17 +4,19 @@ When("I type keyword {string} in the search by address field", (keyword) => {
 });
 
 When("I click item {string} from auto suggession", (keyword) => {
-  //cy.get("#header-search-autosuggest-list li").eq(0).click();
   cy.get('div.pac-item').contains(`${keyword}`).eq(0).click()
 });
 
 When("I click search button", () => {
-  cy.wait(5000);
+  cy.wait(3000);
   cy.get('input[value="Search"]').click();
+  cy.wait(5000);
 });
 
-Then("I should see result pins", () => {
-  cy.contains("mapfiles/transparent.png").should("be.visible");
+Then("I should see result pin icon {string} in map", (keyword) => {
+  cy.get(`div[aria-label="Map"] img[src="https://www.nsw.gov.au/libraries/dcs/images/${keyword}"]`).should("be.visible");
+
+  
 });
 
 When("I only check open space", () => {
@@ -26,8 +28,12 @@ When("I only check open space", () => {
 
 When("I click filter button", () => {
   cy.get(".nsw-filters__accept button").click();
+  cy.wait(5000);
 });
 
-Then("I should see only open space pin", () => {
-  cy.contains("mapfiles/transparent.png").should("be.visible");
+Then("I should not see result pin icon {string} in map", (keyword) => {
+  cy.get(`div[aria-label="Map"] img[src="https://www.nsw.gov.au/libraries/dcs/images/${keyword}"]`).should("not.exist");
+
+  
 });
+
--- cypress/integration/cucumber/FindNSW.feature
+++ cypress/integration/cucumber/FindNSW.feature
@@ -6,7 +6,14 @@ Scenario: Verify result pins in map for places in nsw
    When I type keyword "Haymarket" in the search by address field
    When I click item "Haymarket NSW, Australia" from auto suggession
    When I click search button
-   Then I should see result pins
+   Then I should see result pin icon "public-facility.svg" in map
+   And I should see result pin icon "event.svg" in map
+   And I should see result pin icon "open-space.svg" in map
+   And I should see result pin icon "point-of-interest.svg" in map
    When I only check open space
    When I click filter button
-   Then I should see only open space pin
+   Then I should see result pin icon "open-space.svg" in map
+   And I should not see result pin icon "event.svg" in map
+   And I should not see result pin icon "point-of-interest.svg" in map
+   And I should not see result pin icon "public-facility.svg" in map
+   And I should not see result pin icon "walk.svg" in map
